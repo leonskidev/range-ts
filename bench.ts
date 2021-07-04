58fc58e5e1be18d03aa4eee4e4bda2e2bc46a946
Leon Davis <srcfmt@pm.me>
2021-07-04 08:59:21 +0100

    chore: add more runs to the benchmark

--- bench.ts
+++ bench.ts
@@ -5,7 +5,7 @@ const arr = [1, "hello", 2, "world", 3, "foo", 4, "bar"];
 
 bench({
   name: "native",
-  runs: 10000,
+  runs: 1000000,
   func(b) {
     b.start();
     arr.slice(1, 5);
@@ -15,7 +15,7 @@ bench({
 
 bench({
   name: "range",
-  runs: 10000,
+  runs: 1000000,
   func(b) {
     b.start();
     range("1..5")(arr);
